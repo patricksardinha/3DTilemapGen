c5bf0198552b87108d261bc4f44d5ff20a399afd
patricksardinha <101988616+patricksardinha@users.noreply.github.com>
2023-01-29 11:32:13 +0100

    update displayer inline & random

--- Assets/Scripts/Displayer.cs
+++ Assets/Scripts/Displayer.cs
@@ -10,35 +10,70 @@ public class Displayer : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Start the displayer for a layer.
+    /// </summary>
+    /// <param name="layer">The current layer.</param>
     public void HelloDisplayer(GameObject layer)
     {
-        StartCoroutine(WaitAndDisplayLine(layer));
+        //StartCoroutine(WaitAndDisplayInline(layer, 0.05f));
+        StartCoroutine(WaitAndDisplayRandom(layer, 0.05f));
     }
 
-    private IEnumerator WaitAndDisplayLine(GameObject layer)
+    /// <summary>
+    /// Display line by line the tiles of a layer with a short delay between each tile.
+    /// </summary>
+    /// <param name="layer">The current layer.</param>
+    /// <param name="displayDelay">The delay to display.</param>
+    /// <returns>Yield with delay.</returns>
+    private IEnumerator WaitAndDisplayInline(GameObject layer, float displayDelay)
     {
         foreach (Transform tile in layer.GetComponentsInChildren<Transform>(true))
         {
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(displayDelay);
             tile.gameObject.SetActive(true);
         }
     }
 
-    // TODO: FIX
-    private IEnumerator WaitAndDisplayRandom(GameObject layer)
+    /// <summary>
+    /// Display randomly the tiles of a layer with a short delay between each tile.
+    /// </summary>
+    /// <param name="layer">The current layer.</param>
+    /// <param name="displayDelay">The delay to display.</param>
+    /// <returns>Yield with delay.</returns>
+    private IEnumerator WaitAndDisplayRandom(GameObject layer, float displayDelay)
     {
-        Transform[] inactiveGo = layer.GetComponentsInChildren<Transform>(true);
-        List<GameObject> listInactiveGo = new List<GameObject>();
+        Transform[] inactiveGoInLayer = layer.GetComponentsInChildren<Transform>(true);
+        List<GameObject> listGoInLayer = new List<GameObject>();
 
-        foreach (Transform go in inactiveGo)
+        foreach (Transform go in inactiveGoInLayer)
         {
-            listInactiveGo.Add(go.gameObject);
+            listGoInLayer.Add(go.gameObject);
         }
 
-        foreach (Transform tile in layer.GetComponentsInChildren<Transform>(true))
+        List<GameObject> listGoShuffledInLayer = ShuffleGameobjects(listGoInLayer);
+
+        foreach (GameObject tile in listGoShuffledInLayer)
         {
-            yield return new WaitForSeconds(0.05f);
-            tile.gameObject.SetActive(true);
+            yield return new WaitForSeconds(displayDelay);
+            tile.SetActive(true);
         }
+    }
+
+    /// <summary>
+    /// Shuffle a list of gameobjects.
+    /// </summary>
+    /// <param name="listGo">The initial list of gameobjects.</param>
+    /// <returns>The list randomly shuffled.</returns>
+    private List<GameObject> ShuffleGameobjects(List<GameObject> listGo)
+    {
+        for (int i = 0; i < listGo.Count; i++)
+        {
+            GameObject temp = listGo[i];
+            int randomIndex = Random.Range(i, listGo.Count);
+            listGo[i] = listGo[randomIndex];
+            listGo[randomIndex] = temp;
+        }
+        return listGo;
     }
 }
